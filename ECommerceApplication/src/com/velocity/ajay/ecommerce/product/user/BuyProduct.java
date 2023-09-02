18284fc6a749a2f8b82a17a7faf0769120082e81
AnjaliP <anju.phaltane@gmail.com>
2023-09-02 08:44:59 -0700

    Added code to Update the Product Quantity in BuyProduct Class

--- ECommerceApplication/src/com/velocity/ajay/ecommerce/product/user/BuyProduct.java
+++ ECommerceApplication/src/com/velocity/ajay/ecommerce/product/user/BuyProduct.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 import java.util.Scanner;
 
 public class BuyProduct {
-	public void userBuyProductDetails() {
+	public void userBuyProductDetails() throws SQLException {
 		System.out.println("Enter the Product Id to Buy the Product>> ");
 		Scanner scanner=new Scanner(System.in);
 		int enteredId=scanner.nextInt();
@@ -22,17 +22,62 @@ public class BuyProduct {
 			preparedStatement.setInt(1, enteredId);
 			resultSet=preparedStatement.executeQuery();
 			if(resultSet.next()) {
-				int databaseId=resultSet.getInt("productid");
-				if(enteredId==databaseId) {
-					System.out.println("The Entered ProductId Information is:");
-				}
-			}
+				String productName = resultSet.getString("productname");
+				String productDescription=resultSet.getString("productdescription");
+			    int quantity = resultSet.getInt("productquntity");
+                double price = resultSet.getDouble("productprice");
+            
+
+                // Check if the product is in stock
+                if (quantity > 0) {
+                    System.out.println("Product found: " );
+                    System.out.println("Product Name>>"+ productName);
+                    System.out.println("Product Description>>"+ productDescription);
+                    System.out.println("Available quantity: " + quantity);
+                    System.out.println("Price: $" + price);
+                    
+
+                    // Confirm the purchase
+                    System.out.print("Do you want to purchase this product? (Y/N): ");
+                    char purchaseChoice = scanner.next().charAt(0);
+
+                    if (purchaseChoice == 'Y' || purchaseChoice == 'y') {
+                        // Perform the purchase
+                        // Here, you can update the quantity and perform other necessary operations
+                        // For example, decrement the quantity in the database and record the purchase
+
+                        // Update the product quantity 
+                        String updateQuantityQuery = "UPDATE product SET quantity = ? WHERE product_id = ?";
+                        int updatedQuantity = quantity - 1; // Reduce the quantity by 1
+                        PreparedStatement updateStatement = connection.prepareStatement(updateQuantityQuery);
+                        updateStatement.setInt(1, updatedQuantity);
+                        updateStatement.setInt(2, enteredId);
+                        int rowsUpdated = updateStatement.executeUpdate();
+
+                        if (rowsUpdated > 0) {
+                            System.out.println("Purchase successful. Thank you!");
+                        } else {
+                            System.out.println("Failed to update product quantity. Purchase aborted.");
+                        }
+                    } else {
+                        System.out.println("Purchase aborted.");
+                    }
+                } else {
+                    System.out.println("Sorry, the product is out of stock.");
+                }
+            } else {
+                System.out.println("Product not found with ID: " + enteredId);
+            }
+				
+			
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		finally {
-			
+			connection.close();
+			preparedStatement.close();
+			resultSet.close();
 		}
 		
 		
